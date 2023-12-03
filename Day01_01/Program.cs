4ae5f008e17de68e6f2481da3fc8aa0c1b9a2848
Emyr Griffiths <emyr.griffiths@gmail.com>
2023-12-03 19:27:34 +0000

    Add project files.

---
+++ Day01_01/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] lines = File.ReadAllLines(@"..\..\..\input.txt");
+long total = 0;
+foreach (var line in lines)
+{
+    int firstNum = line.First(line => char.IsDigit(line)) - '0';
+    int lastNum = line.Last(line => char.IsDigit(line)) - '0';
+
+    var number = $"{firstNum}{lastNum}";
+
+    total += int.Parse(number);
+
+}
+
+Console.WriteLine($"Your answer is: {total}");  
+Console.ReadLine();
---
+++ Day02_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+string[] lines = File.ReadAllLines(@"..\..\..\input.txt");
+
+long total = 0;
+var reg = @"\d|one|two|three|four|five|six|seven|eight|nine";
+
+foreach (var line in lines)
+{
+    string firstNum = Regex.Match(line, reg).ToString();
+    var lastNum = Regex.Match(line, reg, RegexOptions.RightToLeft).ToString();
+
+    var number = $"{ParseMatch(firstNum)}{ParseMatch(lastNum)}";
+
+    total += int.Parse(number);
+
+}
+
+int ParseMatch(string st) => st switch
+{
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9,
+    var d => int.Parse(d)
+};
+
+Console.WriteLine($"Your answer is: {total}");
+Console.ReadLine();
